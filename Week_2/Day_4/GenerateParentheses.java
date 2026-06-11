3df0d3d7afe5f3fbf250f640a3021e63405327fa
Ankitrana <ankitrana2807@gmail.com>
2026-06-11 21:07:39 +0530

    Code of Week 2 Day 4

---
+++ Week_2/Day_4/GenerateParentheses.java
@@ -0,0 +1,45 @@
+package Week_2.Day_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+    public static List<String> generateParenthesis(int n) {
+
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+
+        return result;
+    }
+
+    private static void backtrack(List<String> result,
+                                  String current,
+                                  int open,
+                                  int close,
+                                  int n) {
+
+        // Valid combination formed
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        // Add opening bracket
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        // Add closing bracket
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        System.out.println(generateParenthesis(n));
+    }
+}
