6adc68d0a62a4f96790cac7497250f7925ed3a73
Ankitrana <ankitrana2807@gmail.com>
2026-06-15 22:28:46 +0530

    Code of Week 3 Day 1

---
+++ Week_3/Day_1/ReverseLinkedList.java
@@ -0,0 +1,58 @@
+package Week_3.Day_1;
+
+public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode nextNode = curr.next; // store next
+
+            curr.next = prev; // reverse link
+
+            prev = curr; // move prev
+            curr = nextNode; // move curr
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original: ");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.print("Reversed: ");
+        printList(head);
+    }
+}
