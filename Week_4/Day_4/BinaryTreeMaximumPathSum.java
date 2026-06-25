81396173d668e71991e7a0b1697b25dac7f33293
Ankitrana <ankitrana2807@gmail.com>
2026-06-25 11:26:28 +0530

    Code of Week 4 and Day 4

---
+++ Week_4/Day_4/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,48 @@
+package Week_4.Day_4;
+
+public class BinaryTreeMaximumPathSum {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private static int maxSum = Integer.MIN_VALUE;
+
+    public static int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    private static int maxGain(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int currentPathSum = node.val + leftGain + rightGain;
+
+        maxSum = Math.max(maxSum, currentPathSum);
+
+        return node.val + Math.max(leftGain, rightGain);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(-10);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(maxPathSum(root));
+    }
+}
